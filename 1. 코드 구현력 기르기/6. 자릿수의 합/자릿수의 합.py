2b401d47561c0d2e5ca009d19670715b04b99efb
HyeJung <gpwjd248@gmail.com>
2022-04-20 03:11:58 +0900

    1-6: 자릿수의 합 스스로 풀어봄

---
+++ 1. 코드 구현력 기르기/6. 자릿수의 합/자릿수의 합.py
@@ -0,0 +1,18 @@
+import sys
+sys.stdin=open("input.txt", "rt")
+
+n = int(input())
+arr = list(map(str, input().split()))
+
+def digit_sum(x):
+    sum = 0
+    for value in x:
+        sum += int(value)
+    return sum
+
+sumArr = [0] * len(arr)
+for index, value in enumerate(arr):
+    sumArr[index] = digit_sum(value)
+
+index = sumArr.index(max(sumArr))
+print(arr[index])
