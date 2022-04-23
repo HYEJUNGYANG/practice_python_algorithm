daf94d3516a8980a6c1bc0f328f0be9b51906227
HyeJung <gpwjd248@gmail.com>
2022-04-23 22:35:10 +0900

    1-7: 소수 문제 스스로 풀어봄

---
+++ 1. 코드 구현력 기르기/7. 소수/소수.py
@@ -0,0 +1,17 @@
+import sys
+sys.stdin=open("input.txt", "rt")
+
+n = int(input())
+
+primeArr = [1] * (n+1)
+
+for i in range(2, n+1):
+    if primeArr[i]:
+        for j in range(i*i, n+1, i):
+            primeArr[j] = 0  # not primeNum
+
+cnt = -2
+for x in primeArr:
+    if x:
+        cnt += 1
+print(cnt)
