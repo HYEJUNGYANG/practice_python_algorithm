ee90f2357b32cfa298b4520a9773b8ae0fc977db
HyeJung <gpwjd248@gmail.com>
2022-04-23 19:36:06 +0900

    1-6: 문제에 나온 풀이

--- 1. 코드 구현력 기르기/6. 자릿수의 합/자릿수의 합.py
+++ 1. 코드 구현력 기르기/6. 자릿수의 합/자릿수의 합.py
@@ -1,18 +1,42 @@
 import sys
-sys.stdin=open("input.txt", "rt")
+# sys.stdin=open("input.txt", "rt")
+
+# n = int(input())
+# arr = list(input().split())
+
+# def digit_sum(x):
+#     sum = 0
+#     for value in x:
+#         sum += int(value)
+#     return sum
+
+# sumArr = [0] * len(arr)
+# for index, value in enumerate(arr):
+#     sumArr[index] = digit_sum(value)
+
+# index = sumArr.index(max(sumArr))
+# print(arr[index])
+
+# 문제에 나온 풀이
 
 n = int(input())
-arr = list(input().split())
+a = list(map(int, input().split()))
 
 def digit_sum(x):
     sum = 0
-    for value in x:
-        sum += int(value)
+    # while x>0:
+    #     sum += x % 10
+    #     x = x // 10
+    # return sum
+
+    for i in str(x):
+        sum += int(i)
     return sum
 
-sumArr = [0] * len(arr)
-for index, value in enumerate(arr):
-    sumArr[index] = digit_sum(value)
-
-index = sumArr.index(max(sumArr))
-print(arr[index])
+max = 0
+for x in a:
+    tot=digit_sum(x)
+    if(tot > max):
+        max = tot
+        res = x
+print(res)
