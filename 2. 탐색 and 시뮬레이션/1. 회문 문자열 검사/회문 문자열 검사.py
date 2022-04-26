f02fe8bcddfeab5cfccbd36086fd8d57577cdde7
HyeJung <gpwjd248@gmail.com>
2022-04-27 05:25:33 +0900

    1-10: 다른풀이

--- 2. 탐색 and 시뮬레이션/1. 회문 문자열 검사/회문 문자열 검사.py
+++ 2. 탐색 and 시뮬레이션/1. 회문 문자열 검사/회문 문자열 검사.py
@@ -1,20 +1,25 @@
-from pickle import TRUE
 import sys
-
-from numpy import true_divide
 sys.stdin=open("input.txt", "rt")
 
 n = int(input())
 
+# for i in range(n):
+#     string = input()
+#     string = string.lower()
+#     size = len(string)
+#     for x in range(size//2):
+#         if string[x] != string[-(x+1)]:
+#             print("#%d NO" %(i+1))
+#             break
+#     else:
+#         print("#%d YES" %(i+1))
+
+# 다른 풀이
+
 for i in range(n):
-    string = input()
-    string = string.lower()
-    size = len(string)
-    isSame = "YES"   
-    for x in range(size//2):
-        if string[x] == string[-(x+1)]:
-            isSame = "YES"
-        else:
-            isSame = "NO"
-            break
-    print("#", i+1, " ", isSame, sep='')
+    s = input()
+    s = s.lower()
+    if s == s[::-1]:
+        print("#%d YES" %(i+1))
+    else:
+        print("#%d NO" %(i+1))
