50fd1d5e7d1af429cc27a9f3a97b8b9d4384065b
HyeJung <gpwjd248@gmail.com>
2022-04-28 04:13:27 +0900

    2-3: 문제에 나온 풀이

--- 2. 탐색 and 시뮬레이션/3. 카드 역배치/카드 역배치.py
+++ 2. 탐색 and 시뮬레이션/3. 카드 역배치/카드 역배치.py
@@ -1,20 +1,31 @@
 import sys
 sys.stdin=open("input.txt", "rt")
 
-size = 21
-arr = [0] * size
-for i in range(size):
-    arr[i] = i
+# size = 21
+# arr = [0] * size
+# for i in range(size):
+#     arr[i] = i
 
-for x in range(10):
-    ai, bi = map(int, input().split())
-    a = list(arr[ai:bi+1])
-    a = a[::-1]
-    for j in range(ai, bi + 1):
-        arr[j] = a[j-ai]
+# for x in range(10):
+#     ai, bi = map(int, input().split())
+#     a = list(arr[ai:bi+1])
+#     a = a[::-1]
+#     for j in range(ai, bi + 1):
+#         arr[j] = a[j-ai]
 
-for b in arr:
-    if b == 0:
-        continue
-    print(b, end=' ')
-    
+# for b in arr:
+#     if b == 0:
+#         continue
+#     print(b, end=' ')
+
+
+# 문제에 나온 풀이!
+# a, b = b, a -> 파이썬에서의 swap
+a = list(range(21))
+for _ in range(10):  # _로 작성하면 변수가 없이 반복
+    s, e = map(int, input().split())
+    for i in range((e-s+1)//2):
+        a[s+i], a[e-i] = a[e-i], a[s+i]
+a.pop(0)
+for x in a:
+    print(x, end=' ')
