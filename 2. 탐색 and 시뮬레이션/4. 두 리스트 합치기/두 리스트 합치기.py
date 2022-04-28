6bada2c187ce27ce7a7698218e967c8465188766
HyeJung <gpwjd248@gmail.com>
2022-04-28 20:04:24 +0900

    2-4: 문제에 나온 풀이

--- 2. 탐색 and 시뮬레이션/4. 두 리스트 합치기/두 리스트 합치기.py
+++ 2. 탐색 and 시뮬레이션/4. 두 리스트 합치기/두 리스트 합치기.py
@@ -1,13 +1,37 @@
 import sys
 sys.stdin=open("input.txt", "rt")
 
+# n = int(input())
+# nArr = list(map(int, input().split()))
+# m = int(input())
+# mArr = list(map(int, input().split()))
+
+# arr = nArr + mArr
+# arr.sort()
+
+# for x in arr:
+#     print(x, end=' ')
+
+# 문제에 나온 풀이
 n = int(input())
-nArr = list(map(int, input().split()))
+a = list(map(int, input().split()))
 m = int(input())
-mArr = list(map(int, input().split()))
+b = list(map(int, input().split()))
 
-arr = nArr + mArr
-arr.sort()
+p1 = p2 = 0
+c = []
 
-for x in arr:
+while p1<n and p2<m:
+    if a[p1] <= b[p2]:
+        c.append(a[p1])
+        p1 += 1
+    else:
+        c.append(b[p2])
+        p2 += 1
+if p1 < n:
+    c = c + a[p1:]
+if p2 < m:
+    c = c + b[p2:]
+
+for x in c:
     print(x, end=' ')
