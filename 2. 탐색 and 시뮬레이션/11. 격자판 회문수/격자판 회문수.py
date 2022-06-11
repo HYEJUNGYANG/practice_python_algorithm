b08ad28e51796a3b9ea02208c2afed0ddfcf7adf
양혜정 <gpwjd248@gmail.com>
2022-06-12 03:30:20 +0900

    격자판 회문수 - 회문의 길이가 가변적일 때

--- 2. 탐색 and 시뮬레이션/11. 격자판 회문수/격자판 회문수.py
+++ 2. 탐색 and 시뮬레이션/11. 격자판 회문수/격자판 회문수.py
@@ -16,19 +16,37 @@ sys.stdin=open("input.txt", "r")
 # print(cnt)
 
 # 문제에 나온 풀이
+# board = [list(map(int, input().split())) for _ in range(7)]
+# cnt = 0
+# for i in range(3):
+#     for j in range(7):
+#         # 같은 행에서 비교
+#         tmp = board[j][i:i+5]
+#         if tmp == tmp[::-1]:
+#             cnt += 1
+#         # 같은 열에서 비교
+#         for k in range(2):
+#             if board[i+k][j] != board[i+5-k-1][j]:
+#                 break
+#         else:
+#             cnt += 1
+        
+# print(cnt)
+
+# <회문의 길이가 가변적일 때 코드>
 board = [list(map(int, input().split())) for _ in range(7)]
 cnt = 0
+len = 5
 for i in range(3):
     for j in range(7):
-        # 같은 행에서 비교
-        tmp = board[j][i:i+5]
+        tmp = board[j][i:i+len]
         if tmp == tmp[::-1]:
             cnt += 1
-        # 같은 열에서 비교
-        for k in range(2):
-            if board[i+k][j] != board[i+5-k-1][j]:
+        #tmp = board[i:i+5][j] 앞 행은 리스트가 아니라서 슬라이스가 안됨
+        for k in range(len//2):
+            if board[i+k][j] != board[len-k+i-1][j]:
                 break
         else:
-            cnt += 1
+            cnt+=1
         
 print(cnt)
