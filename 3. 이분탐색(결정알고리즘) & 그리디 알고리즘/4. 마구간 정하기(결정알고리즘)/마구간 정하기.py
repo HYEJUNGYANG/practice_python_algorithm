c12d043e2bb47814b75afa8a22b3a8529e7f8a88
양혜정 <gpwjd248@gmail.com>
2022-06-15 15:40:35 +0900

    마구간 정하기 문제에 나온 풀이

--- 3. 이분탐색(결정알고리즘) & 그리디 알고리즘/4. 마구간 정하기(결정알고리즘)/마구간 정하기.py
+++ 3. 이분탐색(결정알고리즘) & 그리디 알고리즘/4. 마구간 정하기(결정알고리즘)/마구간 정하기.py
@@ -1,31 +1,61 @@
 import sys
 sys.stdin=open("input.txt", "r")
 
-n, c = map(int, input().split())
-stables = []
+# n, c = map(int, input().split())
+# stables = []
 
-for _ in range(n):
-    temp = int(input())
-    stables.append(temp)
+# for _ in range(n):
+#     temp = int(input())
+#     stables.append(temp)
 
-stables.sort()
+# stables.sort()
 
-left = 1
-right = stables[-1]
+# left = 1
+# right = stables[-1]
 
-res = 0
-while left <= right:
-    mid = (left + right) // 2
-    p = stables[0]
-    cnt = 0
+# res = 0
+# while left <= right:
+#     mid = (left + right) // 2
+#     p = stables[0]
+#     cnt = 0
+#     for i in range(1, n):
+#         if stables[i] - p < mid:
+#             cnt += 1
+#         else:
+#             p = stables[i]
+#     if (n-cnt) >= c:
+#         res = mid
+#         left = mid + 1
+#     else:
+#         right = mid - 1
+# print(res)
+
+# 문제에 나온 풀이
+def Count(len):
+    cnt = 1
+    ep = Line[0]
     for i in range(1, n):
-        if stables[i] - p < mid:
+        # 나랑 반대로 이렇게 하면 헷갈리지 않게 cnt값 바로 이용하여 풀 수 있음
+        # 나는 (n - cnt)해야 해서 자칫하고 빼먹거나 하면 예상치 못한 결과 나올 수 있음
+        if Line[i]-ep >= len:
             cnt += 1
-        else:
-            p = stables[i]
-    if (n-cnt) >= c:
+            ep = Line[i]
+    return cnt
+
+n, c = map(int, input().split())
+Line = []
+for _ in range(n):
+    tmp = int(input())
+    Line.append(tmp)
+Line.sort()
+lt = 1
+rt = Line[n-1]
+while lt <= rt:
+    mid = (lt + rt) // 2
+    if Count(mid) >= c:
         res = mid
-        left = mid + 1
+        lt = mid + 1
     else:
-        right = mid - 1
+        rt = mid - 1
+
 print(res)
